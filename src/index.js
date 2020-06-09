0ced18bb3b1d7b343aad2e0cc039f997d26d6f56
Ninad <ninadbb@gmail.com>
2020-06-09 12:32:12 +0530

    Extracted App component in saparate App.js file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import App from './App'
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello, React!!!</h1>
-      </div>
-    )
-  }
-}
 
 ReactDOM.render(<App />, document.getElementById('root'))
