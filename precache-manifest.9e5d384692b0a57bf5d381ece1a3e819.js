7d134a31ec67241b1a72cb56a1dba60b8ca6c0c8
Ninad Bulbule <ninadbb@gmail.com>
2020-06-11 23:37:16 +0530

    Updates

---
+++ precache-manifest.9e5d384692b0a57bf5d381ece1a3e819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690b7b7d492982d9a8ebe5131994e78f",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "e13093c88eccfb60516b",
+    "url": "/react-tutorial/static/css/main.fc29d31f.chunk.css"
+  },
+  {
+    "revision": "4762f349b9c98d1da948",
+    "url": "/react-tutorial/static/js/2.0bf4f24d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.0bf4f24d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e13093c88eccfb60516b",
+    "url": "/react-tutorial/static/js/main.689b8b39.chunk.js"
+  },
+  {
+    "revision": "b6f2badd4a096922496c",
+    "url": "/react-tutorial/static/js/runtime-main.ee9240c0.js"
+  }
+]);
